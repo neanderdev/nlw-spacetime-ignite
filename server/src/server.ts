5fa3b144dea8c7bc1d845b9323350d12806f8d0d
Neander de Souza <neander.faria@hotmail.com>
2023-05-22 13:34:01 -0300

    Integrando os projetos web e mobile

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,24 @@
 import 'dotenv/config'
 
+import { resolve } from 'node:path'
+
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import fastify from 'fastify'
 
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
 
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 app.register(cors, {
   origin: true,
 })
@@ -17,6 +27,7 @@ app.register(jwt, {
 })
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
